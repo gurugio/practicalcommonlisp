c99e4b7476b84c84e806ac162998013c3d2a619a
gioh.kim <gurugio@hanmail.net>
2015-12-25 21:36:08 +0100

    on ch3

--- ch3/cd.lisp
+++ ch3/cd.lisp
@@ -3,12 +3,6 @@
 (defun make-cd (title artist rating ripped)
   (list :title title :artist artist :rating rating :ripped ripped))
 
-(getf (make-cd "title" "abc" 1 0) :title)
-(getf (make-cd "title" "abc" 1 0) :rating)
-
-(push (make-cd "aaa" "bbb" 1 t) *db*)
-(push (make-cd "fly" "dixie chicks" 8 t) *db*)
-
 ;; ~{ => argument is a list, loop each element of a list
 ;; ~a => consume one argument and print
 ;; ~10t => emit spaces
@@ -19,5 +13,38 @@
   (dolist (cd *db*)
     (format t "~{~a:~10t~a~%~}~%" cd))) ; t=> *standard-output*
 
+;; *query-io*: stdin
+(defun prompt-read (prompt)
+  (format *query-io* "~a: " prompt)
+  (force-output *query-io*)
+  (read-line *query-io*))
 
+(defun prompt-for-cd ()
+  (make-cd
+   (prompt-read "Title")
+   (prompt-read "Artist")
+   (or (parse-integer (prompt-read "Rating") :junk-allowed t) 0)
+   (y-or-n-p "Ripped [y/n]: ")))
 
+(defun add-record (cd) (push cd *db*))
+
+(defun add-cds ()
+  (loop (add-record (prompt-for-cd))
+     (if (not (y-or-n-p "Another? [y/n]: "))
+         (return))))
+
+(defun save-db (filename)
+  (with-open-file (out filename :direction :output :if-exists :supersede)
+    (with-standard-io-syntax (print *db* out))))
+
+(defun load-db (filename)
+  (with-open-file (in filename)
+    (with-standard-io-syntax (setf *db* (read in)))))
+
+(defun select-by-artist (artist)
+  (remove-if-not #'(lambda (cd) (equal (getf cd :artist) artist))
+                 *db*))
+
+(load-db "./my-cds.db")
+
+(select-by-artist "bbb")
