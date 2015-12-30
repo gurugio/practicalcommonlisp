118b9eeee66469344d600c93f567cf3d61c59c20
Gioh Kim <gurugio@hanmail.net>
2015-12-30 22:52:08 +0100

    ch5: function

--- ch5/ch5.lisp
+++ ch5/ch5.lisp
@@ -3,3 +3,13 @@
 
 (print (print-reverse-array '(1 2 3)))
 (print (print-reverse-array 1))
+
+(defun var-parameter (fir sec &rest others)
+  (format t "FIRST:~a~%" fir)
+  (format t "SECOND:~a~%" sec)
+  (print others))
+
+(format t "first:~a~%" "asdf")
+(var-parameter "abc" "def")
+(var-parameter "abc" "def" "ghi")
+(var-parameter "abc" "def" "ghi" "qwe")
