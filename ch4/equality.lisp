2b91ebbf414d977efce6d4d7088f149a4598be05
Gioh Kim <gi-oh.kim@profitbricks.com>
2015-12-30 17:42:00 +0100

    start ch4, ch5

---
+++ ch4/equality.lisp
@@ -0,0 +1,20 @@
+(print (eq 10 10))
+(print (equal 10 10))
+(print (eq 'a 'a))
+(print (eql 'a 'a))
+(print (equal 'a 'a))
+(print (equalp 'a 'a))
+(print (eq 1 1.0)) ;nil
+(print (eql 1 1.)) ;t
+(print (equal 1 1.0)) ;nil
+(print (equalp 1 1.0)) ;t
+(print (eq '(1 2 3) '(1 2 3)))
+(print (eql '(1 2 3) '(1 2 3)))
+(print (equal '(1 2 3) '(1 2 3)))
+(print (equalp '(1 2 3) '(1 2 3)))
+(print (eq (- 3 2) (- 4 3)))
+(print (eql (- 3 2) (- 4 3)))
+(print (equal (- 3 2) (- 4 3)))
+(print (equalp (- 3 2) (- 4 3)))
+
+
---
+++ ch5/ch5.lisp
@@ -0,0 +1,5 @@
+(defun print-reverse-array (arr)
+  (reverse arr))
+
+(print (print-reverse-array '(1 2 3)))
+(print (print-reverse-array 1))
