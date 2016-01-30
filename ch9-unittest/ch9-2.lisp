6a79d390682ae9c6535d8c011f6195c6a4dfe4f6
gioh.kim <gurugio@hanmail.net>
2016-01-30 17:54:26 +0100

    ch9-unittest: finish

--- ch9-unittest/ch9-2.lisp
+++ ch9-unittest/ch9-2.lisp
@@ -67,4 +67,84 @@
       (= (* 3 5) 15))))
 
 (print (test-arithmetic))
-;;;==================================================
+
+;;; Each test prints the result, test name and equation.
+;; CL-USER> (test-arithmetic)
+;; pass ... TEST-+: (= (+ 1 2) 3)
+;; pass ... TEST-+: (= (+ 1 2 3) 6)
+;; pass ... TEST-+: (= (+ -1 -2) -3)
+;; pass ... TEST-*: (= (* 2 2) 4)
+;; pass ... TEST-*: (= (* 3 5) 15)
+;; T
+;; CL-USER> 
+
+
+;;;========= An Abstraction Emerges ============
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* ',name))
+       ,@body)))
+(print (macroexpand-1 '(deftest test-+ ()
+			(check (= (+ 1 2) 3) (= (+ 3 4) 6)))))
+(deftest test-+ ()
+  (check (= (+ 1 2) 3) (= (+ 2 3) 5)))
+(print (test-+))
+
+(deftest test-* ()
+  (check (= (* 1 2) 2) (= (* 3 4) 12)))
+(print (test-*))
+
+;; high-level test-function can be defined with deftest
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+(print (test-arithmetic))
+
+
+;;;========= A Hierarchy of Tests ==========
+(defvar *test-name* nil)
+
+(defmacro deftest (name parameters &body body)
+  `(defun ,name ,parameters
+     (let ((*test-name* (append *test-name* (list ',name))))
+       ,@body)))
+(print (macroexpand-1 '(deftest test-+ ()
+			(check (= (+ 1 2) 3) (= (+ 3 4) 6)))))
+
+;; The lowest test-function work the same.
+(deftest test-+ ()
+  (check (= (+ 1 2) 3) (= (+ 2 3) 5)))
+(print (test-+))
+
+(deftest test-* ()
+  (check (= (* 1 2) 2) (= (* 3 4) 12)))
+(print (test-*))
+
+(deftest test-arithmetic ()
+  (combine-results
+    (test-+)
+    (test-*)))
+(print (test-arithmetic))
+;;; include high-level test
+;; CL-USER> (test-arithmetic)
+;; pass ... (TEST-ARITHMETIC TEST-+): (= (+ 1 2) 3)
+;; pass ... (TEST-ARITHMETIC TEST-+): (= (+ 2 3) 5)
+;; pass ... (TEST-ARITHMETIC TEST-*): (= (* 1 2) 2)
+;; pass ... (TEST-ARITHMETIC TEST-*): (= (* 3 4) 12)
+;; T
+;; CL-USER> 
+
+(deftest test-math ()
+  (test-arithmetic))
+
+
+;;;======================================
+
+;; test-+ is equivalent to following:
+(let ((ret t))
+  (unless (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3)) (setf ret nil))
+  (unless (report-result (= (+ 2 2) 4) '(= (+ 2 2) 4)) (setf ret nil))
+  (unless (report-result (= (+ 3 2) 5) '(= (+ 3 2) 5)) (setf ret nil))
+  (unless (report-result (= (+ 4 2) 6) '(= (+ 4 2) 6)) (setf ret nil))
+  ret)
