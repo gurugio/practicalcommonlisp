03273a9203979967aca5badd2ea2cef70bc0ce28
gioh.kim <gurugio@hanmail.net>
2016-02-22 18:07:21 +0100

    ch13-beyond-lists: finish

--- ch13-beyond-lists/ch13.lisp
+++ ch13-beyond-lists/ch13.lisp
@@ -34,5 +34,46 @@
 (print *plist*)
 (print (get :a 1)) ; return "information"
 
+;;; destructuring-bind
+(print (destructuring-bind (&key x y z) (list :x 1 :y 2 :z 3)
+	 (+ x y z)))
+
+(print (destructuring-bind (x y z) '(1 2 3)
+	 (list :x x :y y :z z)))
+(print (getf (destructuring-bind (x y z) '(1 2 3)
+	       (list :x x :y y :z z)) :y)) ;2
+(print (destructuring-bind (x y z) (list 1 2 3)
+	 (list :x x :y y :z z)))
+
+(print (destructuring-bind (x y z) '(1 (20 40) 3)
+	 (list :x x :y y :z z))) ; wrong
+(print (getf (destructuring-bind (x y z) '(1 (20 40) 3)
+	       (list :x x :y y :z z)) :y))
+(print (car (getf (destructuring-bind (x y z) '(1 (20 40) 3)
+		   (list :x x :y y :z z)) :y)))
+
+(print (getf (destructuring-bind (x y z) (list 1 (list 20 40) 3)
+	       (list :x x :y y :z z)) :y)) ; return (20 40)
+(print (car (getf (destructuring-bind (x y z) (list 1 (list 20 40) 3)
+		    (list :x x :y y :z z)) :y))) ; return 20
+
+(print (car (getf (destructuring-bind (x y z) '(1 (20 40) 3)
+		    (list :x x :y y :z z)) :y)))
+(print (destructuring-bind (x (y1 y2) z) (list 1 (list 2 20) 3)
+	 (list :x x :y1 y1 :y2 y2 :z z)))
+
+(print (destructuring-bind (x (y1 &optional y2) z) (list 1 (list 2 20) 3)
+	 (list :x x :y1 y1 :y2 y2 :z z)))
+(print (destructuring-bind (x (y1 &optional y2) z) (list 1 (list 2) 3)
+	 (list :x x :y1 y1 :y2 y2 :z z)))
+
+(print (destructuring-bind (&key x y z) (list :x 1 :y 2 :z 3)
+	 (list :x x :y y :z z)))
+(print (destructuring-bind (&key x y z) (list :z 1 :x 2 :y 3)
+	 (list :x x :y y :z z)))
+
+(print (destructuring-bind (&whole whole &key x y z) (list :z 1 :x 2 :y 3)
+	 (list :x x :y y :z z :whole whole)))
+
 
 
