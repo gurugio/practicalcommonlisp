fda8b83e9b56dd2910c5fff1818fa1cbb7b183cd
Gioh Kim <gi-oh.kim@profitbricks.com>
2016-02-29 21:10:35 +0100

    ch16-generic-functions: finish

--- ch16-generic-functions/ch16.lisp
+++ ch16-generic-functions/ch16.lisp
@@ -8,7 +8,14 @@ Signal an error if the current balance is less than amount."))
 (defmethod withdraw ((account bank-account) amount)
   (when (< (balance account) amount)
     (error "Account overdrawn"))
-  (decf (balance account) amount))
+  (decf (balance account) amount)) ; increase amount
 
-    
+(defmethod withdraw ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account)))) ; check overdraft
+    (when (plusp overdraft) ; if overdraft exists
+      (withdraw (overdraft-account account) overdraft) ; withdraw from overdraft-account
+      (incf (balance account) overdraft))) ; add overdraft into current bank-account
+  (call-next-method)) ; go ahead into another method of back-account
+
+
 
