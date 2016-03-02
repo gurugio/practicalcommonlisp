e5a867b3189e2cd56eb7c4872aada4f8e136ae83
gioh.kim <gurugio@hanmail.net>
2016-03-02 19:57:08 +0100

    ch17-classes: on going

--- ch17-classes/ch17-accessor.lisp
+++ ch17-classes/ch17-accessor.lisp
@@ -22,3 +22,20 @@
 (defmethod balance ((account bank-account) &key)
   (slot-value account 'balance))
 (print (balance *account*)) ; get balance
+
+;; define generic function to set customer-name
+;; The name of function is 'LIST' of (setf customer-name)
+(defgeneric (setf customer-name) (value account))
+(defgeneric customer-name (account)) ; w/o &key
+
+;; define method
+;; The name of function should be LIST of setf and customer-name
+(defmethod (setf customer-name) (value (account bank-account))
+  (setf (slot-value account 'customer-name) value))
+(defmethod customer-name ((account bank-account))
+  (slot-value account 'customer-name))
+
+(print (customer-name *account*))
+;; How to call setf function = the same to normal setf function
+(setf (customer-name *account*) "gioh222") ; change name
+(print (customer-name *account*))
