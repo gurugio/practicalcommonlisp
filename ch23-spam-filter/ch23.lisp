5357cf99e6300469f83e480eab17ce8a827eb9a4
Gioh Kim <gurugio@hanmail.net>
2016-03-13 11:03:39 +0100

    ch23-spam-filter: on progress

--- ch23-spam-filter/ch23.lisp
+++ ch23-spam-filter/ch23.lisp
@@ -1,10 +1,28 @@
 
-;;; packages.lisp file includes all packages for spam
-(load "packages.lisp")
 
+;;; I don't know how to use package system yet
+;;; packages.lisp file includes all packages for spam
+;; (load "packages.lisp")
+;; (print (list-directory "/home/gurugio")) ; test including packages
+;; (in-package :com.gigamonkeys.spam)
+
+;;; compile
+(load (compile-file "pathnames.lisp"))
 (print (list-directory "/home/gurugio")) ; test including packages
 
-(in-package :com.gigamonkeys.spam)
+
+;;; spam-filter starts
+
+(defparameter *max-ham-score* .4)
+(defparameter *min-spam-score* .6)
+
+;; top-down approach??
+;; high score is spam
+(defun classification (score)
+  (cond
+    ((<= score *max-ham-score*) 'ham)
+    ((>= score *min-spam-score*) 'spam)
+    (t 'unsure)))
 
 (defun classify (text)
   (classification (score (extract-features text))))
